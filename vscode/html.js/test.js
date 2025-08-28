e8b5ae969fb819ce6e2c6067a102de6c1d5c78f9
Dongseok <zest2700@gmail.com>
2025-08-28 22:43:40 +0900

    test commit

--- vscode/html.js/test.js
+++ vscode/html.js/test.js
@@ -1,1 +1,34 @@
-console.log("테스트테스트트");
+class people    
+{
+    age = 0;
+    name = "";
+
+    constructor(age, name)
+    {
+        this.age = age;
+        this.name = name;
+    }
+
+    showname()
+    {
+        console.log('나는 people class 입니다.');
+    }
+}
+
+class mangbung extends people
+{
+    constructor(age, name)
+    {
+        super(age, name);
+    }
+
+    // @override
+    showname() 
+    {
+        console.log('나는 mangbung class 입니다.');
+    }
+}
+
+let testmangbung = new mangbung('34', 'mangbung');
+
+testmangbung.showname();
